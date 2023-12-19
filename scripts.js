24ca113709f7355a9fc4634be60929aca5383834
Yousif Ebrahim <yousif.ebrahim@proton.me>
2023-12-20 02:51:11 +0300

    changes again

--- scripts.js
+++ scripts.js
@@ -1,24 +1,12 @@
 function showGame(index) {
     const games = document.querySelectorAll('.game');
-    games.forEach(game => game.style.display = 'none');
+    games.forEach(game => {
+        game.style.display = 'none';
+    });
 
     const selectedGame = document.getElementById(`game-${index}`);
     if (selectedGame) {
         selectedGame.style.display = 'block';
-    }
-
-    const videos = [
-        'https://www.youtube.com/embed/3nD3sYWy250', // Outlast Trials
-        'https://www.youtube.com/embed/thKNo9ZT0PI', // Mortal Kombat
-        'https://www.youtube.com/embed/hOpPwTapTfI', // Crime Boss: Rockay City
-        'https://www.youtube.com/embed/91w-eRbS6g0', // Little Nightmares II
-        'https://www.youtube.com/embed/uubDhQZ-U64', // Garry's Mod
-        'https://www.youtube.com/embed/MKDQCmhK2Ws', // Ready or Not
-    ];
-
-    const videoFrame = document.getElementById(`video-${index}`);
-    if (videoFrame) {
-        videoFrame.src = videos[index];
     }
 
     const listItems = document.querySelectorAll('li');
@@ -28,3 +16,4 @@ function showGame(index) {
         selectedListItem.classList.add('active');
     }
 }
+
