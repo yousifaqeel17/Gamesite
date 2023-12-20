05191d505676a22372ff59b9d0c9e967997c626d
Yousif Ebrahim <yousif.ebrahim@proton.me>
2023-12-20 03:00:34 +0300

    script changes

--- scripts.js
+++ scripts.js
@@ -9,6 +9,20 @@ function showGame(index) {
         selectedGame.style.display = 'block';
     }
 
+    const videoSources = [
+        'https://www.youtube.com/embed/3nD3sYWy250', // Outlast Trials
+        'https://www.youtube.com/embed/thKNo9ZT0PI', // Mortal Kombat
+        'https://www.youtube.com/embed/hOpPwTapTfI', // Crime Boss: Rockay City
+        'https://www.youtube.com/embed/91w-eRbS6g0', // Little Nightmares II
+        'https://www.youtube.com/embed/uubDhQZ-U64', // Garry's Mod
+        'https://www.youtube.com/embed/MKDQCmhK2Ws', // Ready or Not
+    ];
+
+    const videoFrame = document.getElementById(`video-${index}`);
+    if (videoFrame) {
+        videoFrame.src = videoSources[index];
+    }
+
     const listItems = document.querySelectorAll('li');
     listItems.forEach(item => item.classList.remove('active'));
     const selectedListItem = document.getElementById(`option-${index}`);
